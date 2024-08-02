d3c85586b0803afb6a59aa9a1c36f4569da96b4b
dimbolasamuel <dimbola.samuel@gmail.com>
2024-08-02 17:27:32 -0400

    Update security.tf

--- security.tf
+++ security.tf
@@ -1,41 +1,59 @@
 resource "aws_wafv2_web_acl" "example_acl" {
   name        = "example-web-acl"
-  scope       = "REGIONAL" # or "CLOUDFRONT"
+  scope       = "REGIONAL"
   description = "Example Web ACL"
 
   rule {
-    name     = "sql-injection-rule"
+    name     = "block-post-requests-content-type-application-json"
     priority = 1
-    action   = {
+
+    action {
       block {}
     }
+
     statement {
-      byte_match_statement {
-        search_string = "SELECT"
-        field_to_match {
-          query_string {}
-        }
-        text_transformations {
-          type = "LOWERCASE"
-        }
-        positional_constraint = "CONTAINS"
+      and_statement {
+        statements = [
+          {
+            byte_match_statement {
+              field_to_match {
+                method {}
+              }
+              positional_constraint = "CONTAINS"
+              search_string = "POST"
+              text_transformation {
+                priority = 0
+                type = "NONE"
+              }
+            }
+          },
+          {
+            not_statement {
+              statement {
+                byte_match_statement {
+                  search_string = "application/json"
+                  field_to_match {
+                    single_header {
+                      name = "content-type"
+                    }
+                  }
+                  positional_constraint = "CONTAINS"
+                  text_transformation {
+                    priority = 0
+                    type = "NONE"
+                  }
+                }
+              }
+            }
+          }
+        ]
       }
     }
+
     visibility_config {
       sampled_requests_enabled = true
       cloudwatch_metrics_enabled = true
-      metric_name = "sqlInjectionMetric"
+      metric_name = "webAclMetric"
     }
   }
-
-  default_action {
-    allow {}
-  }
-
-  visibility_config {
-    sampled_requests_enabled = true
-    cloudwatch_metrics_enabled = true
-    metric_name = "webAclMetric"
-  }
 }
-
